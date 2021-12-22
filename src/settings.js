3dd19fa100a62aeea02ef11d11ec5576910049de
Alexander Pihl <cph-as509@cphbusiness.dk>
2021-12-22 12:11:17 +0100

    settings change

--- src/settings.js
+++ src/settings.js
@@ -1,26 +1,23 @@
-//export const URL = "https://www.jplm.dk/CA3_Boilerplate_Backend/";
-export const URL = "http://localhost:8080/Boilerplate-Backend";
-
-//export https://www.jplm.dk/Boilerplate-Backend/api/kubernetes/namespaces
+//Localhost URLs
+/* export const URL = "http://localhost:8080/Boilerplate-Backend";
 export const URLNamespaces = "http://localhost:8080/Boilerplate-Backend/api/kubernetes/namespaces";
-
-//export https://www.jplm.dk/Boilerplate-Backend/api/kubernetes/deployments
 export const URLDeployments = "http://localhost:8080/Boilerplate-Backend/api/kubernetes/deployments";
-
-//export https://www.jplm.dk/Boilerplate-Backend/api/kubernetes/services
 export const URLServices = "http://localhost:8080/Boilerplate-Backend/api/kubernetes/services";
-
-//export https://www.jplm.dk/Boilerplate-Backend/api/kubernetes/pods
 export const URLPods = "http://localhost:8080/Boilerplate-Backend/api/kubernetes/pods";
-
-//export https://www.jplm.dk/Boilerplate-Backend/api/kubernetes/pods
 export const URLApps = "http://localhost:8080/Boilerplate-Backend/api/apps/";
-
-//export https://www.jplm.dk/Boilerplate-Backend/api/kubernetes/pods
 export const URLAppsUpdate = "http://localhost:8080/Boilerplate-Backend/api/apps/update/";
-
-//export https://www.jplm.dk/Boilerplate-Backend/api/kubernetes/pods
 export const URLAppsDelete = "http://localhost:8080/Boilerplate-Backend/api/apps/delete/";
+export const URLUser = "http://localhost:8080/Boilerplate-Backend/api/users/username/"; */
 
-//export https://www.jplm.dk/Boilerplate-Backend/api/kubernetes/pods
-export const URLUser = "http://localhost:8080/Boilerplate-Backend/api/users/username/";
+//----------------------------------------------------------------------------------------------------//
+
+//Deployed URLs
+export const URL = "https://www.jplm.dk/CA3_Boilerplate_Backend/";
+export const URLNamespaces = "https://www.jplm.dk/Boilerplate-Backend/api/kubernetes/namespaces";
+export const URLDeployments = "https://www.jplm.dk/Boilerplate-Backend/api/kubernetes/deployments";
+export const URLServices = "https://www.jplm.dk/Boilerplate-Backend/api/kubernetes/services";
+export const URLPods = "https://www.jplm.dk/Boilerplate-Backend/api/kubernetes/pods";
+export const URLApps = "https://www.jplm.dk/Boilerplate-Backend/api/apps/";
+export const URLAppsUpdate = "https://www.jplm.dk/Boilerplate-Backend/api/apps/update/";
+export const URLAppsDelete = "https://www.jplm.dk/Boilerplate-Backend/api/apps/delete/";
+export const URLUser = "https://www.jplm.dk/Boilerplate-Backend/api/users/username/";
